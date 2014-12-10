487102ebba989417d4f442ba52a468503ce2ce7d
lagarcia <lagarcia@L3221.MEDIOAMBIENTE.GOV.AR>
2014-12-10 14:57:35 -0300

    Se agregaron las entidades Departamento, Localidad, Municipio, Provincia

---
+++ src/main/java/ar/gob/ambiente/servicios/gestionterritorial/entidades/Departamento.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ar.gob.ambiente.servicios.gestionterritorial.entidades;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author epassarelli
+ */
+@Entity
+public class Departamento implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+
+
+
+    @ManyToOne
+    @JoinColumn(name="provincia_id")
+    private Provincia provincia;
+        
+    @OneToMany(mappedBy="departamento")
+    private List<Localidad> localidades;    
+    
+    @OneToMany(mappedBy="departamento")
+    private List<Municipio> municipios;     
+
+    @OneToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+    @JoinColumn(name="adminentidad_id")
+    private AdminEntidad adminentidad; 
+
+    @OneToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+    @JoinColumn(name="georef_id")
+    private GeoRef georef; 
+    
+    public Departamento(){
+        localidades = new ArrayList();
+        municipios = new ArrayList();
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }    
+    
+    public Provincia getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(Provincia provincia) {
+        this.provincia = provincia;
+    }
+
+    public List<Localidad> getLocalidades() {
+        return localidades;
+    }
+
+    public void setLocalidades(List<Localidad> localidades) {
+        this.localidades = localidades;
+    }
+
+    public List<Municipio> getMunicipios() {
+        return municipios;
+    }
+
+    public void setMunicipios(List<Municipio> municipios) {
+        this.municipios = municipios;
+    }    
+    
+    public AdminEntidad getAdminentidad() {
+        return adminentidad;
+    }
+
+    public void setAdminentidad(AdminEntidad adminentidad) {
+        this.adminentidad = adminentidad;
+    }
+
+    public GeoRef getGeoref() {
+        return georef;
+    }
+
+    public void setGeoref(GeoRef georef) {
+        this.georef = georef;
+    }    
+    
+    
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Departamento)) {
+            return false;
+        }
+        Departamento other = (Departamento) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ar.gob.ambiente.servicios.gestionterritorial.entidades.Departamento[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/main/java/ar/gob/ambiente/servicios/gestionterritorial/entidades/Municipio.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ar.gob.ambiente.servicios.gestionterritorial.entidades;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author epassarelli
+ */
+@Entity
+public class Municipio implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    //private Provincia provincia;
+
+    @ManyToOne
+    @JoinColumn(name="departamento_id")
+    private Departamento departamento;
+        
+    @OneToMany(mappedBy="municipio")
+    private List<Localidad> localidades;
+    
+    @OneToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+    @JoinColumn(name="adminentidad_id")
+    private AdminEntidad adminentidad; 
+
+    @OneToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+    @JoinColumn(name="georef_id")
+    private GeoRef georef; 
+    
+    public Municipio(){
+        localidades = new ArrayList();
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public List<Localidad> getLocalidades() {
+        return localidades;
+    }
+
+    public void setLocalidades(List<Localidad> localidades) {
+        this.localidades = localidades;
+    }
+
+    public AdminEntidad getAdminentidad() {
+        return adminentidad;
+    }
+
+    public void setAdminentidad(AdminEntidad adminentidad) {
+        this.adminentidad = adminentidad;
+    }
+
+    public GeoRef getGeoref() {
+        return georef;
+    }
+
+    public void setGeoref(GeoRef georef) {
+        this.georef = georef;
+    }    
+
+    
+    
+    
+
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Municipio)) {
+            return false;
+        }
+        Municipio other = (Municipio) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ar.gob.ambiente.servicios.gestionterritorial.entidades.Municipio[ id=" + id + " ]";
+    }
+    
+}
