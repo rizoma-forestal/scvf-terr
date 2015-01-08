e457c6e978eb1247cb8caab0d41f8db470d368aa
Ruben Incostante <rincostante@ambiente.gob.ar>
2015-01-08 13:30:14 -0300

    Se eliminó la infraestructura de Arquillian para los test con el glassfish incrustado, se actualizaron todos los facades y el POM. También se pasó la versión de PromeFaces a 5.0

--- src/main/java/ar/gob/ambiente/servicios/gestionterritorial/facades/AbstractFacade.java
+++ src/main/java/ar/gob/ambiente/servicios/gestionterritorial/facades/AbstractFacade.java
@@ -9,9 +9,6 @@ package ar.gob.ambiente.servicios.gestionterritorial.facades;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import javax.persistence.TemporalType;
 
 /**
