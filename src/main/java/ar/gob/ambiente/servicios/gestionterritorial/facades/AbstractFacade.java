e0f6f631776658f0b5e151683f96615cbfce29ab
webpass <epassarelli@gmail.com>
2015-02-13 15:33:07 -0300

    Se modificó el Facade de EspRegion para que verifique si ademas de tener dependencias, estan Habilitadas

--- src/main/java/ar/gob/ambiente/servicios/gestionterritorial/facades/AbstractFacade.java
+++ src/main/java/ar/gob/ambiente/servicios/gestionterritorial/facades/AbstractFacade.java
@@ -44,7 +44,7 @@ public abstract class AbstractFacade<T> {
     public void remove(T entity) {
         getEntityManager().remove(getEntityManager().merge(entity));
     }
-
+    
     public T find(Object id) {
         return getEntityManager().find(entityClass, id);
     }
