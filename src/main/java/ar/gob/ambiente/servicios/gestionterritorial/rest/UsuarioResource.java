da705d2f6cebec3ff7d18a5cb4434f9fc84ffc3d
Ruben Incostante <rincostante@ambiente.gob.ar>
2018-03-14 14:13:45 -0300

    Se actualizó la validación del Usuario para la API

--- src/main/java/ar/gob/ambiente/servicios/gestionterritorial/rest/UsuarioResource.java
+++ src/main/java/ar/gob/ambiente/servicios/gestionterritorial/rest/UsuarioResource.java
@@ -27,9 +27,7 @@ public class UsuarioResource {
     private UsuarioFacade usFacade;    
     
     /**
-     * @param user
-     * @return 
-     * @api {get} /login/ Obtiene un Response con el token incluído en el header
+     * @api {get} /login/ Obtener token
      * @apiVersion 1.0.0
      * @apiName GetUsuario
      * @apiGroup Usuario
